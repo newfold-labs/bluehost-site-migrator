41c205f1412f39e7e9235af7f0837b2a8edd17f6
Micah Wood <micah@wpscholar.com>
2020-06-19 12:48:40 -0400

    Properly output JSON

--- includes/class-migration-checks.php
+++ includes/class-migration-checks.php
@@ -79,7 +79,7 @@ class BH_Site_Migrator_Migration_Checks {
 				$body        = wp_remote_retrieve_body( $response );
 				$data        = json_decode( $body, true );
 				if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-					file_put_contents( BH_Site_Migrator_Utilities::get_upload_path( 'manifestScanApiResponse.json' ), $data ); // phpcs:ignore
+					file_put_contents( BH_Site_Migrator_Utilities::get_upload_path( 'manifestScanApiResponse.json' ), wp_json_encode( $data, JSON_PRETTY_PRINT ) ); // phpcs:ignore
 				}
 				if ( 200 === $status_code && isset( $data, $data['feasible'], $data['migrationId'], $data['x-auth-token'] ) ) {
 					if ( isset( $data['factors'] ) ) {
