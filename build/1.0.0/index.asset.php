140c8334dfc9723671ee8f2565da0368d5365f7c
amartya-dev <amarkaushik1999@gmail.com>
2023-08-30 17:21:25 +0530

    remove 1.0.1 from build

--- build/1.0.0/index.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array( 'lodash', 'react', 'wp-dom-ready', 'wp-element' ),
-	'version'      => 'ba3bb207e849aa51584f',
-);
--- build/1.0.12/bh-site-migrator.asset.php
+++ build/1.0.12/bh-site-migrator.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element' ),
-	'version'      => '0e84bc3eeeda56a70960',
-);
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element'), 'version' => '8ee6c215e7840dbb27ec');
